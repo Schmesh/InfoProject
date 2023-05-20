8ac24e08fd0c522c162940a71f030b1b72efe4c0
Schmesh <vincent.fender@web.de>
2023-05-20 15:31:50 +0200

    Hitboxes collison finished

--- Classes/Hitbox.java
+++ Classes/Hitbox.java
@@ -18,11 +18,28 @@ public class Hitbox {
         points[7].setXyz(Pos.getXYZ()[0]+(xyzSize[0]/2), Pos.getXYZ()[1]+(xyzSize[1]/2), Pos.getXYZ()[2]+(xyzSize[2]/2) );
     }
 
-    public Point returnPoint(int index){
+    public Point getPoint(int index){
         return points[index];
     }
 
     public boolean checkCollision(Point point){
+        if (    point.getXYZ()[0]>points[0].getXYZ()[0] && point.getXYZ()[0]<points[4].getXYZ()[0] &&   //Control X Overlap
+                point.getXYZ()[1]>points[0].getXYZ()[1] && point.getXYZ()[1]<points[3].getXYZ()[1] &&   //Control Y Overlap
+                point.getXYZ()[2]>points[1].getXYZ()[2] && point.getXYZ()[2]<points[2].getXYZ()[2]){    //Control Z Overlap
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 
+    public boolean checkCollision(Hitbox hitbox){
+        boolean b = false;
+        for (int i =0; i<8; i++){
+            if (checkCollision(hitbox.getPoint(i))){
+                b=true;
+            }
+        }
+        return b;
     }
 }
