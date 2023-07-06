30f24164105a569d1b1fedc9ccbd87cd347db1ba
TimoCremer <timo.cremer7@gmail.com>
2023-07-06 16:43:30 +0200

    added map class

---
+++ Classes/Map.java
@@ -0,0 +1,15 @@
+package Classes;
+
+import java.util.ArrayList;
+
+public class Map {
+    ArrayList<Hitbox> Cuboids= new ArrayList();
+    Map(){
+        Cuboids.add(new Hitbox(new double[]{1,1,1},new double[]{0/*x*/,0/*y*/,20/*z*/}));
+
+
+
+
+
+    }
+}
