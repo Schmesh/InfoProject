046478f7f5c0abcd39aa2373860464cb1d6a2c3b
TimoCremer <timo.cremer7@gmail.com>
2023-07-06 16:55:25 +0200

    new cuboids

--- Classes/Map.java
+++ Classes/Map.java
@@ -6,6 +6,16 @@ public class Map {
     ArrayList<Hitbox> Cuboids= new ArrayList();
     Map(){
         Cuboids.add(new Hitbox(new double[]{1,1,1},new double[]{0/*x*/,0/*y*/,20/*z*/}));
+        Cuboids.add(new Hitbox(new double[]{1,1,1},new double[]{0/*x*/,1/*y*/,20/*z*/}));
+        Cuboids.add(new Hitbox(new double[]{1,1,1},new double[]{0/*x*/,2/*y*/,20/*z*/}));
+        Cuboids.add(new Hitbox(new double[]{1,1,1},new double[]{0/*x*/,3/*y*/,20/*z*/}));
+        Cuboids.add(new Hitbox(new double[]{1,1,1},new double[]{0/*x*/,4/*y*/,20/*z*/}));
+        Cuboids.add(new Hitbox(new double[]{1,1,1},new double[]{0/*x*/,5/*y*/,20/*z*/}));
+        Cuboids.add(new Hitbox(new double[]{1,1,1},new double[]{0/*x*/,6/*y*/,20/*z*/}));
+        Cuboids.add(new Hitbox(new double[]{1,1,1},new double[]{0/*x*/,7 /*y*/,20/*z*/}));
+        Cuboids.add(new Hitbox(new double[]{1,1,1},new double[]{0/*x*/,8/*y*/,20/*z*/}));
+        Cuboids.add(new Hitbox(new double[]{1,1,1},new double[]{0/*x*/,9/*y*/,20/*z*/}));
+        Cuboids.add(new Hitbox(new double[]{1,1,1},new double[]{0/*x*/,10/*y*/,20/*z*/}));
 
 
 
